8e3d8be534a3b961104ea61643ad3fcb3fbacab2
kirstendanica <kirdanic@gmail.com>
2023-12-22 19:53:59 -0500

    Reformat all files for better readability, fix bug affecting snake speed and direction, update styling

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
+// Define directions as constants
+const DIRECTIONS = {
+    UP: "up",
+    DOWN: "down",
+    LEFT: "left",
+    RIGHT: "right"
+};
+
 // Game class
 class Game {
     constructor(gridSize, tileSize) {
         this.gridSize = gridSize;
         this.tileSize = tileSize;
-        this.snake = [{ x: gridSize / 2, y: gridSize / 2 }, { x: gridSize / 2, y: gridSize / 2 + 1 }, { x: gridSize / 2, y: gridSize / 2 + 2 }];
-        this.direction = "up";
+        this.restartGame();
+        this.highestScore = 0;
+        this.direction = DIRECTIONS.RIGHT; 
+    }
+
+    restartGame() {
+        const midPoint = Math.floor(this.gridSize / 2);
+        this.snake = [{x: midPoint, y: midPoint}, {x: midPoint, y: midPoint + 1}, {x: midPoint, y: midPoint + 2}];
+        this.direction = DIRECTIONS.RIGHT;
         this.food = this.getRandomFoodPosition();
+        this.score = 0;
+        this.updateScoreBoard();
     }
 
     handleGameOver() {
         alert("Game Over! Your snake collided with its own tail. Better luck next time!");
+        gameApp.stop();
         this.restartGame();
-    }
+      }
+    
 
     moveSnake() {
-        const head = { x: this.snake[0].x, y: this.snake[0].y };
+        const head = Object.assign({}, this.snake[0]);
 
         // Check for collision with the snake's tail
         if (this.isCollidingWithSnakeTail(head)) {
@@ -23,10 +42,20 @@ class Game {
         }
 
         // Update head position based on direction
-        if (this.direction === "up") head.y -= 1;
-        if (this.direction === "down") head.y += 1;
-        if (this.direction === "left") head.x -= 1;
-        if (this.direction === "right") head.x += 1;
+        switch (this.direction) {
+            case DIRECTIONS.UP: 
+                head.y -= 1;
+                break;
+            case DIRECTIONS.DOWN: 
+                head.y += 1;
+                break;
+            case DIRECTIONS.LEFT: 
+                head.x -= 1;
+                break;
+            case DIRECTIONS.RIGHT: 
+                head.x += 1;
+                break;
+        }
 
         // Check for collision with defined boundary
         if (this.isCollidingWithBoundary(head)) {
@@ -46,18 +75,23 @@ class Game {
         this.snake.unshift(head);
     }
 
-    restartGame() {
-        this.snake = [{ x: this.gridSize / 2, y: this.gridSize / 2 }, { x: this.gridSize / 2, y: this.gridSize / 2 + 1 }, { x: this.gridSize / 2, y: this.gridSize / 2 + 2 }];
-        this.direction = "up";
-        this.food = this.getRandomFoodPosition();
-    }
-
-    isCollidingWithBoundary(position) {
-        return position.x < 0 || position.x >= this.gridSize || position.y < 0 || position.y >= this.gridSize;
-    }
-
+    isCollidingWithBoundary(t) {
+        return t.x < 0 || t.x >= this.gridSize || t.y < 0 || t.y >= this.gridSize;
+      }
+      
     isCollidingWithFood() {
-        return this.snake[0].x === this.food.x && this.snake[0].y === this.food.y;
+        if (this.snake[0].x === this.food.x && this.snake[0].y === this.food.y) {
+            this.score += 10;
+            this.highestScore = Math.max(this.highestScore, this.score);
+            this.updateScoreBoard();
+            return true;
+        }
+        return false;
+    }
+
+    updateScoreBoard() {
+        const scoreBoard = document.getElementById('scoreBoard');
+        scoreBoard.textContent = `Score: ${this.score} | Highest Score: ${this.highestScore}`;
     }
 
     isCollidingWithSnakeTail(head) {
@@ -70,24 +104,28 @@ class Game {
     }
 
     getRandomFoodPosition() {
-        return { x: Math.floor(Math.random() * this.gridSize), y: Math.floor(Math.random() * this.gridSize) };
+        return {
+            x: Math.floor(Math.random() * this.gridSize),
+            y: Math.floor(Math.random() * this.gridSize)
+        };
     }
 }
 
 // Renderer class
 class Renderer {
-    constructor(canvasContext, tileSize) {
+    constructor(canvasContext, tileSize, game) {
         this.ctx = canvasContext;
         this.tileSize = tileSize;
+        this.game = game;
     }
 
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 
-    drawSnake(snake) {
-        snake.forEach((segment, index) => {
-            const color = index === 0 ? "#546e7a" : (index === snake.length - 1 ? "#90a4ae" : "#78909c");
+    drawSnake() {
+        this.game.snake.forEach((segment, index) => {
+            const color = index === 0 ? "#546e7a" : (index === this.game.snake.length - 1 ? "#90a4ae" : "#78909c");
             this.ctx.fillStyle = color;
             this.ctx.fillRect(segment.x * this.tileSize, segment.y * this.tileSize, this.tileSize, this.tileSize);
             this.ctx.strokeStyle = "black";
@@ -95,16 +133,15 @@ class Renderer {
         });
     }
 
-    drawFood(food) {
+    drawFood() {
         this.ctx.fillStyle = "#e57373";
-        this.ctx.fillRect(food.x * this.tileSize, food.y * this.tileSize, this.tileSize, this.tileSize);
+        this.ctx.fillRect(this.game.food.x * this.tileSize, this.game.food.y * this.tileSize, this.tileSize, this.tileSize);
     }
 
     drawScores() {
-        this.ctx.fillStyle = "#333";
-        this.ctx.font = "16px Arial";
-        this.ctx.fillText(`Score: ${game.score}`, 10, 20);
-        this.ctx.fillText(`Highest Score: ${game.highestScore}`, 10, 40);
+        this.ctx.fillStyle = "#333";this.ctx.font = "16px Arial";
+        this.ctx.fillText(`Score: ${this.game.score}`, 10, 20);
+        this.ctx.fillText(`Highest Score: ${this.game.highestScore}`, 10, 40);
     }
 }
 
@@ -117,15 +154,31 @@ class UserInput {
 
     bindEvents() {
         document.addEventListener("keydown", (e) => {
-            const directions = { ArrowUp: "up", ArrowDown: "down", ArrowLeft: "left", ArrowRight: "right" };
-            if (directions[e.key] && this.game.direction !== directions[e.key]) this.game.direction = directions[e.key];
+            const directions = {
+                ArrowUp: DIRECTIONS.UP,
+                ArrowDown: DIRECTIONS.DOWN,
+                ArrowLeft: DIRECTIONS.LEFT,
+                ArrowRight: DIRECTIONS.RIGHT
+            };
+
+            // Prevent the snake from moving in the opposite direction immediately
+            if (directions[e.key]) {
+                const newDirection = directions[e.key];
+                const { direction } = this.game;
+                if (
+                    (direction === DIRECTIONS.UP && newDirection !== DIRECTIONS.DOWN) ||
+                    (direction === DIRECTIONS.DOWN && newDirection !== DIRECTIONS.UP) ||
+                    (direction === DIRECTIONS.LEFT && newDirection !== DIRECTIONS.RIGHT) ||
+                    (direction === DIRECTIONS.RIGHT && newDirection !== DIRECTIONS.LEFT)
+                ) {
+                    this.game.direction = newDirection;
+                }
+            }
         });
     }
 }
-
-// GameApp class
-class GameApp {
-    constructor(canvas) {
+class GameApp{
+    constructor(canvas){
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
         this.gridSize = 20;
@@ -133,24 +186,25 @@ class GameApp {
         this.canvas.width = this.gridSize * this.tileSize;
         this.canvas.height = this.gridSize * this.tileSize;
         this.game = new Game(this.gridSize, this.tileSize);
-        this.renderer = new Renderer(this.ctx, this.tileSize);
+        this.renderer = new Renderer(this.ctx, this.tileSize, this.game);
         this.userInput = new UserInput(this.game);
+        this.gameSpeed = 100;  // You can adjust this value to control the speed of the game
     }
-
-    start() {
-        this.gameLoop();
+    start(){
+        this.gameInterval = setInterval(() => this.gameLoop(), this.gameSpeed);
     }
-
-    gameLoop() {
+    gameLoop(){
         this.renderer.clearCanvas();
         this.game.moveSnake();
-        this.renderer.drawSnake(this.game.snake);
-        this.renderer.drawFood(this.game.food);
-        setTimeout(() => this.gameLoop(), 100);
+        this.renderer.drawSnake();
+        this.renderer.drawFood();
+        this.renderer.drawScores();
+    }
+    stop() {
+        clearInterval(this.gameInterval);
     }
 }
 
-// Initialize game on the "gameCanvas" element
 const canvas = document.getElementById("gameCanvas");
 const gameApp = new GameApp(canvas);
 gameApp.start();
